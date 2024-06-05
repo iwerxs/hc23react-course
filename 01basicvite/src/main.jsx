a38f9950f8c96977bfa3f290f83c924a4ad6fb0e
mlaane <goobric@gmail.com>
2024-06-05 16:52:23 +0100

    updated the main & App jsx files

--- 01basicvite/src/main.jsx
+++ 01basicvite/src/main.jsx
@@ -3,8 +3,32 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+
+const reactElement = {
+  type: 'a',
+  props: {
+    href: 'https://google.com',
+    target: '_blank',
+  },
+  children: 'Click me to visit google',
+};
+
+function MyApp() {
+  return (
+    <div>
+      <h1>Custome React App</h1>
+    </div>
+  );
+}
+const AnotherElement = (
+  <a href='https://google.com' target='_blank'>
+    Google
+  </a>
+);
+const areactElement = React.createElement(
+  'a',
+  { href: 'https://google.com', target: '_blank' },
+  'Click me to visit google'
+);
+
+ReactDOM.createRoot(document.getElementById('root')).render(areactElement);
